9b60cf23715af2b47e02cd610d0c9490acb872b7
Vladislav Stroev <137615800+NupaZzz@users.noreply.github.com>
2025-02-05 10:50:36 +0300

    Delete lesson-1/target directory

--- lesson-1/target/debug/deps/lesson_1-7e0c3bbf2a6c4a71.d
+++
@@ -1,5 +0,0 @@
-D:\rust-learning\lesson-1\target\debug\deps\liblesson_1-7e0c3bbf2a6c4a71.rmeta: src\main.rs
-
-D:\rust-learning\lesson-1\target\debug\deps\lesson_1-7e0c3bbf2a6c4a71.d: src\main.rs
-
-src\main.rs:
